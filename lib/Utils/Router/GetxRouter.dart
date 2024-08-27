19c98a5b4c2e7510e7fc0fec17244d71d22df147
vishesh <visheshpatel2610@gmail.com>
2024-08-27 15:16:10 +0530

    update -- update rebase completed

--- lib/Utils/Router/GetxRouter.dart
+++ lib/Utils/Router/GetxRouter.dart
@@ -3,6 +3,10 @@ import 'package:get/get.dart';
 import 'package:trip_expance_manager/Bindings/AccountScreenBinding.dart';
 import 'package:trip_expance_manager/Bindings/ProfileScreenBinding.dart';
 import 'package:trip_expance_manager/Utils/Router/RouteName.dart';
+import 'package:trip_expance_manager/View/Auth/LogInScreen.dart';
+import 'package:trip_expance_manager/View/Auth/SignUpScreen.dart';
+import 'package:trip_expance_manager/View/BottomNavigationBar/BottomBar.dart';
+import 'package:trip_expance_manager/View/HomeScreen/HomeScreen.dart';
 import 'package:trip_expance_manager/View/IntroScreen/IntroScreen.dart';
 import 'package:trip_expance_manager/View/ProfileScreen/Account/AccountScreen.dart';
 import 'package:trip_expance_manager/View/ProfileScreen/ProfileScreen.dart';
@@ -13,6 +17,30 @@ Route<dynamic>? generateRoute(RouteSettings settings) {
       return getPageRoutes(
           routeName: RoutesName.introScreen,
           page: () => const IntroScreen(),
+          settings: settings,
+          bindings: []);
+    case RoutesName.loginScreen:
+      return getPageRoutes(
+          routeName: RoutesName.loginScreen,
+          page: () => const LogInScreen(),
+          settings: settings,
+          bindings: []);
+    case RoutesName.signUpScreen:
+      return getPageRoutes(
+          routeName: RoutesName.signUpScreen,
+          page: () => const SignUpScreen(),
+          settings: settings,
+          bindings: []);
+    case RoutesName.bottomBarScreen:
+      return getPageRoutes(
+          routeName: RoutesName.bottomBarScreen,
+          page: () => const BottomBar(),
+          settings: settings,
+          bindings: []);
+    case RoutesName.homeScreen:
+      return getPageRoutes(
+          routeName: RoutesName.homeScreen,
+          page: () => const HomeScreen(),
           settings: settings,
           bindings: []);
     case RoutesName.profileScreen:
@@ -27,18 +55,6 @@ Route<dynamic>? generateRoute(RouteSettings settings) {
           page: () => const AccountScreen(),
           settings: settings,
           bindings: [ProfileScreenBinding(), AccountScreenBinding()]);
-    case RoutesName.profileScreen:
-      return getPageRoutes(
-          routeName: RoutesName.profileScreen,
-          page: () => const ProfileScreen(),
-          settings: settings,
-          bindings: [ProfileScreenBinding()]);
-    case RoutesName.accountScreen:
-      return getPageRoutes(
-          routeName: RoutesName.accountScreen,
-          page: () => const AccountScreen(),
-          settings: settings,
-          bindings: []);
   }
 }
 
